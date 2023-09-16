efe05f1e32e7a267f4665df9704da70b13fe3ee1
pathik <pathikchauhan17@gmail.com>
2023-09-16 11:24:01 +0530

    ss

--- client/src/components/home/func/useChange.js
+++ client/src/components/home/func/useChange.js
@@ -5,6 +5,7 @@ export const useChange = () => {
     const func = () => {
       const height = window.innerHeight
       document.getElementsByClassName("home")[0].style.height = `${height}px`
+      console.log(height)
     }
     func()
     window.addEventListener("resize", func)
